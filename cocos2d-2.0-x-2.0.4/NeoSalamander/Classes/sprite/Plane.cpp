e0128a8b03617f48c8d7323351011ff3d65b5572
Austin <austin_chang@sohu.com>
2012-12-03 21:44:19 +0800

    add the plane class with the very basic functions

--- cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/sprite/Plane.cpp
+++ cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/sprite/Plane.cpp
@@ -1,10 +1,35 @@
 #include "Plane.h"
+#include "SimpleAudioEngine.h"
+using namespace cocos2d;
 
 
 Plane::Plane(void)
 {
 }
 
+Plane::Plane(char* sprite)
+{
+	if(!sprite)
+		m_Sprite = NULL;
+
+	m_Sprite = CCSprite::create(sprite);
+}
+
+CCSprite* Plane::GetSprite()
+{
+    return m_Sprite;
+}
+
+void Plane::SetPostion(int x,int y)
+{
+	m_Sprite->setPosition(ccp(x,y));
+}
+
+void Plane::AddToLayer(CCLayer* layer)
+{
+	assert(layer!=NULL);
+	layer->addChild(m_Sprite);
+}
 
 Plane::~Plane(void)
 {
--- cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/sprite/Plane.h
+++ cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/sprite/Plane.h
@@ -1,8 +1,23 @@
-#pragma once
+#ifndef __PLANE_H__
+#define __PLANE_H__
+
+#include "cocos2d.h"
+#include "Box2D/Box2D.h"
+#include "ZSprite.h"
+using namespace cocos2d;
+
 class Plane
 {
 public:
 	Plane(void);
+	Plane(char* sprite);//Pass the sprite file name to this constructor 
+	CCSprite* GetSprite();//Get the pointer to the plane sprite
+	void SetPostion(int x,int y);//Set the postion of the sprite in the layer
+	void AddToLayer(CCLayer* layer);//The plane add itself to a layer
 	~Plane(void);
+
+protected:
+	CCSprite* m_Sprite;
 };
 
+#endif
