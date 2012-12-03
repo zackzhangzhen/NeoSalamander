602a3fce88cc5a780187338116d1c8d8406fe8a9
Austin <austin_chang@sohu.com>
2012-12-03 23:52:26 +0800

    add anim feature for plane

--- cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/sprite/Plane.cpp
+++ cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/sprite/Plane.cpp
@@ -5,6 +5,9 @@ using namespace cocos2d;
 
 Plane::Plane(void)
 {
+	m_curAnim = NULL;
+	m_Sprite = NULL;
+	m_curLayer = NULL;
 }
 
 Plane::Plane(char* sprite)
@@ -13,6 +16,10 @@ Plane::Plane(char* sprite)
 		m_Sprite = NULL;
 
 	m_Sprite = CCSprite::create(sprite);
+
+	m_Sprite->setPosition(ccp(0,0));
+	m_curAnim = NULL;
+	m_curLayer = NULL;
 }
 
 CCSprite* Plane::GetSprite()
@@ -20,15 +27,44 @@ CCSprite* Plane::GetSprite()
     return m_Sprite;
 }
 
-void Plane::SetPostion(int x,int y)
+void Plane::SetPosition(int x,int y)
 {
 	m_Sprite->setPosition(ccp(x,y));
+}
+
+CCPoint Plane::GetPostion()
+{
+	return m_Sprite->getPosition();
 }
 
 void Plane::AddToLayer(CCLayer* layer)
 {
 	assert(layer!=NULL);
 	layer->addChild(m_Sprite);
+	m_curLayer = layer;
+}
+
+void Plane::AddAnimation(string animName,CCRepeatForever* anim)
+{
+	m_AnimMap.insert(map<string,CCRepeatForever*>::value_type(animName,anim));
+}
+
+void Plane::PlayAnimation(string animName)
+{
+	if(m_curLayer == NULL)
+	{
+		printf("This target has not been added to any layer yet");
+		return;
+	}
+
+	map<string,CCRepeatForever*>::iterator it = m_AnimMap.find(animName);
+	assert(it != m_AnimMap.end());
+
+	if(m_curAnim != NULL)
+		m_Sprite->stopAction(m_curAnim);
+	m_curAnim = it->second;
+	m_Sprite->runAction(it->second);
+
 }
 
 Plane::~Plane(void)
--- cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/sprite/Plane.h
+++ cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/sprite/Plane.h
@@ -4,20 +4,35 @@
 #include "cocos2d.h"
 #include "Box2D/Box2D.h"
 #include "ZSprite.h"
-using namespace cocos2d;
+#include <map>
+#include <string>
+using namespace cocos2d; 
 
+/*
+ Plane* pl = new Plane("picName");
+ pl->SetPosition(100,100);
+ pl->AddToLayer(layPtr);
+ pl->AddAnimation("anim1",Utility::getAnimationAction("pre","ext",5,true));
+ pl->PlayAnimation("anim1");
+*/
 class Plane
 {
 public:
 	Plane(void);
 	Plane(char* sprite);//Pass the sprite file name to this constructor 
 	CCSprite* GetSprite();//Get the pointer to the plane sprite
-	void SetPostion(int x,int y);//Set the postion of the sprite in the layer
+	void SetPosition(int x,int y);//Set the postion of the sprite in the layer
+	CCPoint GetPostion();
 	void AddToLayer(CCLayer* layer);//The plane add itself to a layer
+	void AddAnimation(string animName,CCRepeatForever* anim);//Add an animation to the plane's anim map
+	void PlayAnimation(string animName);//Play an animation of the plane,designated by the animName
 	~Plane(void);
 
 protected:
 	CCSprite* m_Sprite;
+	map<string,CCRepeatForever*> m_AnimMap;
+	CCRepeatForever* m_curAnim;
+    CCLayer* m_curLayer;
 };
 
 #endif
