de03e6c086204976908d5d505c14d4e8b06938be
Austin <austin_chang@sohu.com>
2012-12-08 23:28:05 +0800

    add collision system

--- cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/collision/CollisionDetector.cpp
+++ cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/collision/CollisionDetector.cpp
@@ -1,6 +1,8 @@
 #include "CollisionDetector.h"
+extern int kkk;
+map<string,CollisionHandler*> collHandlers;
 
-ColObjArray::ColObjArray(void)
+CollObjArray::CollObjArray(void)
 {
    m_Tag = "";
    m_objArr = NULL;
@@ -8,10 +10,80 @@ ColObjArray::ColObjArray(void)
 
 CollisionDetector::CollisionDetector(void)
 {
-
 }
 
 
 CollisionDetector::~CollisionDetector(void)
 {
+}
+
+
+void CollisionDetector::AddToCollArray(CollObjArray* _collObjArray)
+{
+	m_AllCollObj.push_back(_collObjArray);
+}
+
+void CollisionDetector::AddToTargetMap(string& tag, vector<string>& vec)
+{
+	m_tgtMap.insert(map<string,vector<string>>::value_type(tag,vec));
+}
+
+CollObjArray* CollisionDetector::FindArrayByTag(string tag)
+{
+	for(int i = 0;i<m_AllCollObj.size();i++)
+	{
+		if(tag == m_AllCollObj[i]->m_Tag)
+		{
+		    return m_AllCollObj[i];
+		}			
+	}
+}
+
+void CollisionDetector::CollideTwoArrays(CollObjArray* arrLeft,CollObjArray* arrRight)
+{
+	for(int i = 0;i<arrLeft->m_objArr->count();i++)
+	{
+		CCObject* leftObj = arrLeft->m_objArr->data->arr[i];
+		CCObject* rightObj = NULL;
+		CCSprite* leftSprite = (CCSprite*)leftObj;
+		if(left == NULL)
+			continue;
+		CCSprite* rightSprite = NULL;
+		CCRect leftRect;
+		CCRect rightRect;
+		leftRect = CCRectMake(leftSprite->getPosition().x - (leftSprite->getContentSize().width/2),leftSprite->getPosition().y - (leftSprite->getContentSize().height/2),leftSprite->getContentSize().width,leftSprite->getContentSize().height);
+
+		for(int j = 0; j<arrRight->m_objArr->count();j++)
+		{
+			rightObj = arrRight->m_objArr->data->arr[j];
+			rightSprite = (CCSprite*)rightObj;
+			if(right == NULL)
+				continue; 
+		    rightRect = CCRectMake(rightSprite->getPosition().x - (rightSprite->getContentSize().width/2),rightSprite->getPosition().y - (rightSprite->getContentSize().height/2),rightSprite->getContentSize().width,rightSprite->getContentSize().height);
+
+			if (CCRect::CCRectIntersectsRect(leftRect, rightRect))
+			{
+				string str = arrLeft->m_Tag + "_"+arrRight->m_Tag;
+				map<string,CollisionHandler*>::iterator it = collHandlers.find(str);
+				it->second->HandleCollison(it->second->m_Layer,leftObj,rightObj);
+			}
+		}
+	}
+}
+
+
+void CollisionDetector::Detect()
+{
+	if(kkk==2)
+		kkk = 3;
+	for(map<string,vector<string>>::iterator it1 = m_tgtMap.begin();it1 != m_tgtMap.end();it1++)
+	{
+		CollObjArray* collSide1 = FindArrayByTag(it1->first);
+		CollObjArray* collSide2 = NULL;
+		for(vector<string>::iterator it2 = it1->second.begin();it2!=it1->second.end();it2++)
+		{
+		    collSide2 = FindArrayByTag(*it2);
+			CollideTwoArrays(collSide1,collSide2);
+		}	
+	}
 }
--- cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/collision/CollisionDetector.h
+++ cocos2d-2.0-x-2.0.4/NeoSalamander/Classes/collision/CollisionDetector.h
@@ -2,16 +2,25 @@
 #include "cocos2d.h"
 #include "Box2D/Box2D.h"
 #include <string>
+#include <map>
+#include <vector>
 using namespace cocos2d;
 
-class ColObjArray
+class CollObjArray
 {
 public:
-	ColObjArray(void);
-	~ColObjArray(void);
+	CollObjArray(void);
+	~CollObjArray(void);
 
 	string m_Tag;
 	CCArray* m_objArr;
+};
+
+class CollisionHandler
+{
+public:
+	virtual void HandleCollison(CCLayer* layer,CCObject* obj1, CCObject* obj2) = 0; 
+	CCLayer* m_Layer;
 };
 
 class CollisionDetector
@@ -19,5 +28,20 @@ class CollisionDetector
 public:
 	CollisionDetector(void);
 	~CollisionDetector(void);
+	void AddToCollArray(CollObjArray* _collObjArray);
+	void AddToTargetMap(string& tag, vector<string>& vec);
+	void Detect();
+
+	//void (__thiscall *test_handler)(CCNode*,CCNode*);
+
+protected:
+	CollObjArray* FindArrayByTag(string tag);
+	void CollideTwoArrays(CollObjArray* arrLeft,CollObjArray* arrRight);
+
+	//map<string,void (CCNode*,CCNode*)> collHandlerMap;
+	vector<CollObjArray*> m_AllCollObj;
+	map<string,vector<string>> m_tgtMap;
 };
 
+
+ //map<string,CollisionHandler*> collHandlers;
